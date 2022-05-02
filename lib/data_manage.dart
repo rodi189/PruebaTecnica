bbec971f1c7c4a55d15794bb96f87ed0b7050b57
Alexander Barragán <rodihacker@gmail.com>
2022-05-01 21:21:39 -0500

    Creación función conbinarDatos

---
+++ lib/data_manage.dart
@@ -0,0 +1,31 @@
+import 'api.dart';
+import 'firestore_db.dart';
+
+List listPokemons = [];
+Map mapPokemons = {};
+
+void conbinarDatos() {
+  listPokemons = [];
+  mapPokemons = {};
+  mapPokemonsFirestore.forEach((key, value) {
+    if (mapPokemonsAPI.containsKey(key) == true) {
+      Map auxDataPokemonFirestore = mapPokemonsFirestore[key];
+      mapPokemons[key] = auxDataPokemonFirestore;
+      listPokemons.add(auxDataPokemonFirestore);
+    } else {
+      mapPokemons[key] = value;
+      listPokemons.add(value);
+    }
+  });
+  try {
+    listPokemons.sort((a, b) {
+      String pokedexA = a["pokedex"];
+      String pokedexB = b["pokedex"];
+      int valueA = int.tryParse(pokedexA) ?? 0;
+      int valueB = int.tryParse(pokedexB) ?? 0;
+      return valueA.compareTo(valueB);
+    });
+  } catch (e) {
+    print("error sort pokemons $e");
+  }
+}
